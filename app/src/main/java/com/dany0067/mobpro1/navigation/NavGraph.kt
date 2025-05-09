966b97875ca29b600dd994e23e2b1342e980ce3d
danyirfansyah <danyirfansyah23@gmail.com>
2025-05-09 21:13:35 +0700

    Membuat FormBaru dan FormUbah, dan memanggil KEY_ID_CATATAN

--- app/src/main/java/com/dany0067/mobpro1/navigation/NavGraph.kt
+++ app/src/main/java/com/dany0067/mobpro1/navigation/NavGraph.kt
@@ -1,7 +1,6 @@
 package com.dany0067.mobpro1.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
