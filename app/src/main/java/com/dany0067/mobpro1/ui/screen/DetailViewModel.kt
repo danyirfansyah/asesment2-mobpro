5177719e29d7400b70c74c6e0c1dc2c6c5c09405
danyirfansyah <danyirfansyah23@gmail.com>
2025-05-09 21:31:42 +0700

    Menambahkan fungsi untuk mengelola data catatan dan kontak, termasuk operasi insert, update, delete

--- app/src/main/java/com/dany0067/mobpro1/ui/screen/DetailViewModel.kt
+++ app/src/main/java/com/dany0067/mobpro1/ui/screen/DetailViewModel.kt
@@ -1,47 +1,65 @@
 package com.dany0067.mobpro1.ui.screen
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dany0067.mobpro1.database.CatatanDao
 import com.dany0067.mobpro1.model.Catatan
+import com.dany0067.mobpro1.model.CatatanWithTelefon
+import com.dany0067.mobpro1.model.Kontak
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val dao: CatatanDao) : ViewModel() {
 
-    fun insert(nama: String, nim: String, kelas: String) {
+    fun insert(nama: String, nim: String, jenisKelamin: String, noHp: String, email: String) {
         val catatan = Catatan(
             nama = nama,
-            NIM   = nim,
-            kelas = kelas
+            NIM = nim,
+            jenisKelamin = jenisKelamin
+        )
+
+        val kontak = Kontak(
+            noHp = noHp,
+            email = email,
+            catatanId = 0L
         )
 
         viewModelScope.launch(Dispatchers.IO) {
-            dao.insert(catatan)
+            dao.insertCatatanWithKontak(catatan, kontak)
         }
     }
 
-    suspend fun getCatatan(id: Long): Catatan? {
-        return dao.getCatatanById(id)
-    }
-
-    fun update(id: Long, nama: String, NIM: String, kelas: String) {
+    fun update(id: Long, nama: String, nim: String, jenisKelamin: String, noHp: String, email: String) {
         val catatan = Catatan(
-            id  = id,
-            nama   = nama,
-            NIM = NIM,
-            kelas = kelas
+            id = id,
+            nama = nama,
+            NIM = nim,
+            jenisKelamin = jenisKelamin
+        )
+
+        val kontak = Kontak(
+            id = id,
+            noHp = noHp,
+            email = email,
+            catatanId = id
         )
 
         viewModelScope.launch(Dispatchers.IO) {
-            dao.update(catatan)
+            Log.d("DetailViewModel", "Updating Catatan: $catatan")
+            Log.d("DetailViewModel", "Updating Kontak: $kontak")
+
+            dao.updateCatatanWithKontak(catatan, kontak)
         }
     }
 
     fun delete(id: Long) {
         viewModelScope.launch(Dispatchers.IO) {
-            dao.deleteById(id)
+            dao.deleteCatatanWithKontak(id)
         }
     }
 
+    suspend fun getCatatanWithKontak(id: Long): CatatanWithTelefon? {
+        return dao.getCatatanWithKontak(id)
+    }
 }
