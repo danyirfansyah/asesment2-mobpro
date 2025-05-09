1137f98e0b26767c48db212b39b85d384eb203ad
danyirfansyah <danyirfansyah23@gmail.com>
2025-05-09 21:17:32 +0700

    Menambahkan Insert Update Kontak, dan Query Kontak

--- app/src/main/java/com/dany0067/mobpro1/database/CatatanDao.kt
+++ app/src/main/java/com/dany0067/mobpro1/database/CatatanDao.kt
@@ -3,25 +3,64 @@ package com.dany0067.mobpro1.database
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
 import com.dany0067.mobpro1.model.Catatan
+import com.dany0067.mobpro1.model.CatatanWithTelefon
+import com.dany0067.mobpro1.model.Kontak
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CatatanDao {
 
     @Insert
-    suspend fun insert(catatan: Catatan)
+    suspend fun insert(catatan: Catatan): Long
+
+    @Insert
+    suspend fun insert(kontak: Kontak): Long
 
     @Update
     suspend fun update(catatan: Catatan)
 
-    @Query("SELECT * FROM catatan ORDER BY id DESC")
-    fun getCatatan(): Flow<List<Catatan>>
+    @Update
+    suspend fun updateKontak(kontak: Kontak)
 
-    @Query("SELECT * FROM catatan WHERE id =:id")
-    suspend fun getCatatanById(id: Long) : Catatan?
+    @Transaction
+    suspend fun updateCatatanWithKontak(catatan: Catatan, kontak: Kontak) {
+        update(catatan)
+        updateKontak(kontak)
+    }
+
+    @Transaction
+    suspend fun insertCatatanWithKontak(catatan: Catatan, kontak: Kontak) {
+        val catatanId = insert(catatan)
+        kontak.catatanId = catatanId
+        insert(kontak)
+    }
+
+    @Transaction
+    @Query("SELECT * FROM catatan WHERE id = :id")
+    suspend fun getCatatanWithKontak(id: Long): CatatanWithTelefon?
+
+    @Transaction
+    @Query("SELECT * FROM catatan")
+    fun getCatatanWithKontak(): Flow<List<CatatanWithTelefon>>
+
+    @Query("SELECT * FROM catatan WHERE id = :id")
+    suspend fun getCatatanById(id: Long): Catatan?
+
+    @Query("SELECT * FROM kontak WHERE catatanId = :catatanId")
+    suspend fun getKontakByCatatanId(catatanId: Long): Kontak?
 
     @Query("DELETE FROM catatan WHERE id = :id")
     suspend fun deleteById(id: Long)
+
+    @Query("DELETE FROM kontak WHERE catatanId = :catatanId")
+    suspend fun deleteKontakByCatatanId(catatanId: Long)
+
+    @Transaction
+    suspend fun deleteCatatanWithKontak(id: Long) {
+        deleteKontakByCatatanId(id)
+        deleteById(id)
+    }
 }
