740ddbb17f2568fc858eb6601403ad62152cda5b
danyirfansyah <danyirfansyah23@gmail.com>
2025-05-09 21:16:06 +0700

    Memanggil database CatatanDao dan Telefon

--- app/src/main/java/com/dany0067/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/dany0067/mobpro1/ui/screen/MainViewModel.kt
@@ -3,16 +3,15 @@ package com.dany0067.mobpro1.ui.screen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dany0067.mobpro1.database.CatatanDao
-import com.dany0067.mobpro1.model.Catatan
+import com.dany0067.mobpro1.model.CatatanWithTelefon
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 
-class MainViewModel(dao: CatatanDao) : ViewModel(){
-    val data: StateFlow<List<Catatan>> = dao.getCatatan().stateIn(
+class MainViewModel(dao: CatatanDao) : ViewModel() {
+    val data: StateFlow<List<CatatanWithTelefon>> = dao.getCatatanWithKontak().stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
         initialValue = emptyList()
     )
 }
-
